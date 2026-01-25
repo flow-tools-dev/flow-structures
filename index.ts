c8ca09ea1e765cc34f6044a4f77f8314740129af
Adam Allison <allisonadam81@gmail.com>
2026-01-24 17:10:12 -0800

    done

--- index.ts
+++ index.ts
@@ -1,137 +1,214 @@
-type ListOrArr<T> = T[] | FlowList<T>;
-
-class FlowList<T> extends Array<T> {
-  constructor(...n: any) {
-    super(...n);
+class FlowList<T> {
+  private array: T[];
+  constructor(array: T[]) {
+    this.array = array;
   }
 
-  static get [Symbol.species]() {
-    return FlowList;
-  }
-
-  static of<T>(...items: T[]) {
-    const f = new FlowList<T>();
-    f.push(...items);
-    return f;
+  static of<T>(items: T[]) {
+    return new FlowList(items);
   }
 
   static isFlowList(item: any) {
     return item instanceof FlowList;
   }
 
-  static empty(n: number) {
-    return new FlowList(n);
+  get length() {
+    return this.array.length;
   }
 
-  map<U>(
-    callback: (value: T, index: number, array: FlowList<T>) => U,
-    thisArg?: any,
+  keys(): IterableIterator<number> {
+    return this.array.keys();
+  }
+
+  values(): IterableIterator<T> {
+    return this.array.values();
+  }
+
+  entries(): IterableIterator<[number, T]> {
+    return this.array.entries();
+  }
+
+  pop() {
+    return this.array.pop();
+  }
+
+  push(...items: T[]) {
+    return this.array.push(...items);
+  }
+
+  shift() {
+    return this.array.shift();
+  }
+
+  unshift(...items: T[]) {
+    return this.array.unshift(...items);
+  }
+
+  map<U>(fn: (value: T, index: number, list: FlowList<T>) => U): FlowList<U> {
+    return FlowList.of<U>(this.array.map((v, i) => fn(v, i, this)));
+  }
+
+  flatMap<U>(
+    fn: (value: T, index: number, list: FlowList<T>) => U | U[] | FlowList<U>,
   ): FlowList<U> {
-    return super.map(
-      callback as (value: T, index: number, array: T[]) => U,
-      thisArg,
-    ) as FlowList<U>;
+    const out: U[] = [];
+    this.array.forEach((value, index) => {
+      const r = fn(value, index, this);
+      if (Array.isArray(r)) {
+        out.push(...r);
+      } else if (r instanceof FlowList) {
+        out.push(...r.toArray());
+      } else {
+        out.push(r);
+      }
+    });
+    return FlowList.of<U>(out);
   }
 
-  // @ts-expect-error
   filter(
-    predicate: (value: T, index: number, array: FlowList<T>) => boolean,
-    thisArg?: any,
+    predicate: (value: T, index: number, list: FlowList<T>) => boolean,
   ): FlowList<T> {
-    return super.filter(
-      predicate as (value: T, index: number, array: T[]) => boolean,
-      thisArg,
-    ) as FlowList<T>;
+    return FlowList.of(this.array.filter((v, i) => predicate(v, i, this)));
   }
 
-  toReversed() {
-    return FlowList.of<T>(...super.toReversed());
+  reduce<U>(
+    fn: (acc: U, curr: T, index: number, list: FlowList<T>) => U,
+    init: U,
+  ): U {
+    return this.array.reduce((acc, curr, i) => fn(acc, curr, i, this), init);
+  }
+
+  some(fn: (value: T, index: number, list: FlowList<T>) => boolean): boolean {
+    return this.array.some((v, i) => fn(v, i, this));
+  }
+
+  every(fn: (value: T, index: number, list: FlowList<T>) => boolean): boolean {
+    return this.array.every((v, i) => fn(v, i, this));
+  }
+
+  find(
+    fn: (value: T, index: number, list: FlowList<T>) => boolean,
+  ): T | undefined {
+    return this.array.find((v, i) => fn(v, i, this));
+  }
+
+  findIndex(
+    fn: (value: T, index: number, list: FlowList<T>) => boolean,
+  ): number {
+    return this.array.findIndex((v, i) => fn(v, i, this));
+  }
+
+  includes(value: T): boolean {
+    return this.array.includes(value);
+  }
+
+  at(index: number): T | undefined {
+    return this.array.at(index);
+  }
+
+  toReverse() {
+    return FlowList.of<T>(this.array.toReversed());
   }
 
   toSorted(fn?: (a: T, b: T) => number) {
-    return FlowList.of<T>(...super.toSorted(fn));
+    return FlowList.of<T>(this.array.toSorted(fn));
   }
 
   toSpliced(start: number, deleteCount?: number, ...items: T[]): FlowList<T> {
     // @ts-expect-error
-    return FlowList.of<T>(...super.toSpliced(start, deleteCount, ...items));
+    return FlowList.of<T>(this.array.toSpliced(start, deleteCount, ...items));
   }
+
   slice(start?: number, end?: number): FlowList<T> {
-    return FlowList.of<T>(...super.slice(start, end));
+    return FlowList.of<T>(this.array.slice(start, end));
   }
 
-  concat(...items: (T | ListOrArr<T>)[]): FlowList<T> {
-    const arr: T[] = [];
-    items.forEach((item) => {
-      Array.isArray(item)
-        ? arr.push(...item)
-        : FlowList.isFlowList(item)
-          ? arr.push(...item)
-          : arr.push(item);
-    });
-    return FlowList.of<T>(...arr);
+  concat(...items: ConcatArray<T>[]): FlowList<T> {
+    return FlowList.of(this.array.concat(...items));
   }
 
-  // @ts-expect-error
   flat(depth?: number): FlowList<T> {
-    return FlowList.of<T>(...(super.flat(depth) as any));
+    // @ts-expect-error
+    return FlowList.of<T>(this.array.flat(depth));
+  }
+
+  [Symbol.iterator](): Iterator<T> {
+    return this.array[Symbol.iterator]();
+  }
+
+  append(...items: T[]) {
+    return FlowList.of<T>([...this.array, ...items]);
+  }
+
+  prepend(...items: T[]) {
+    return FlowList.of<T>([...items, ...this.array]);
   }
 
   batch(n: number) {
-    const agg: FlowList<T>[] = [];
-    let chunk: FlowList<T> = FlowList.of<T>();
-    this.forEach((el) => {
+    const agg: T[][] = [];
+    let chunk: T[] = [];
+    this.array.forEach((el) => {
       if (chunk.length < n) chunk.push(el);
       if (chunk.length === n) {
         agg.push(chunk);
-        chunk = FlowList.of<T>();
+        chunk = [];
       }
     });
     if (chunk.length) agg.push(chunk);
-    return FlowList.of(...agg);
+    return FlowList.of<T[]>(agg);
   }
 
   compact() {
-    return FlowList.of(...this.filter(Boolean));
+    return FlowList.of<T>(this.array.filter(Boolean));
   }
 
   dedupe() {
-    return FlowList.of(...new Set(this));
+    return FlowList.of<T>(Array.from(new Set(this.array)));
   }
 
   head() {
-    return this.at(0);
+    return this.array[0];
   }
 
   tail() {
-    return this.at(this.length - 1);
+    return this.array[this.array.length - 1];
   }
 
-  difference(...arrs: ListOrArr<T>[]) {
+  difference(...arrs: T[][]) {
     const set = new Set(arrs.flat());
-    return FlowList.of(...this.filter((el) => !set.has(el)));
+    return FlowList.of<T>(this.array.filter((el) => !set.has(el)));
   }
 
-  xor(...arrs: ListOrArr<T>[]) {
-    const sets = [this, ...arrs].map((a) => new Set(a));
+  xor(...arrs: T[][]) {
+    const sets = [this.array, ...arrs].map((a) => new Set(a));
     const result = sets
       .flatMap((s) => [...s])
       .filter((el) => sets.filter((s) => s.has(el)).length === 1);
-    return FlowList.of(...result);
+    return FlowList.of<T>(result);
   }
 
-  union(...arrs: ListOrArr<T>[]) {
-    return FlowList.of(...this, ...arrs.flat()).dedupe();
+  union(...arrs: T[][]) {
+    return FlowList.of([...this.array, ...arrs.flat()]).dedupe();
+  }
+
+  partition(
+    predicate: (value: T, index: number, list: FlowList<T>) => boolean,
+  ): FlowList<T[]> {
+    const pass: T[] = [];
+    const fail: T[] = [];
+
+    this.array.forEach((value, index) => {
+      (predicate(value, index, this) ? pass : fail).push(value);
+    });
+    return FlowList.of<T[]>([pass, fail]);
+  }
+
+  toArray() {
+    return this.array;
   }
 }
-const q = FlowList.of(1, 2, 3, 4, 4, 4)
-  .toReversed()
-  .toSorted()
-  .batch(2)
-  .flat()
-  .batch(2)
-  .map((fl) => fl.head());
+const q = FlowList.of([1, 2]).toSorted().batch(2).flat().batch(2);
 const a = [1, 2, 3];
-const b = [4, 5, 6].map;
+const b = [4, 5, 6];
 
-console.log();
+console.log(q);
